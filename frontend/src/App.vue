452b95220608bdd270219ce282ec8b11764995c0
Tom-js-python <tomrembrown@proton.me>
2024-08-12 18:18:28 -0400

    Now loads all data into the table

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,15 +3,18 @@ import TheMenuBar from './components/TheMenuBar.vue'
 import TheLinksTable from './components/TheLinksTable.vue'
 import { ref } from 'vue'
 
-const API_URL = 'http://127.0.0.1:5000/test'
+const API_URL = 'http://127.0.0.1:5000/'
 const data_message = ref('No Links Are Loaded')
+const news_links = ref([])
 
 async function onToggle(isLoaded) {
   data_message.value = 'Scraping Hacker News Site...'
   if (isLoaded) {
     let raw_data = await fetch(API_URL)
     let data = await raw_data.json()
-    data_message.value = data.Message
+    data_message.value = 'Loaded'
+    news_links.value = data.NewsLinks
+    console.log(news_links.value)
   } else {
     data_message.value = 'No Links Are Loaded'
   }
@@ -23,7 +26,7 @@ async function onToggle(isLoaded) {
 
   <main>
     <TheMenuBar @toggle="onToggle" />
-    <TheLinksTable :data-message="data_message" />
+    <TheLinksTable :data-message="data_message" :news-links="news_links" />
   </main>
 </template>
 
--- frontend/src/components/TheLinksTable.vue
+++ frontend/src/components/TheLinksTable.vue
@@ -1,9 +1,58 @@
 <script setup>
 const props = defineProps({
-  dataMessage: String
+  dataMessage: String,
+  newsLinks: Object
 })
 </script>
 
 <template>
-  <p>{{ dataMessage }}</p>
+  <p v-if="dataMessage != 'Loaded'">{{ dataMessage }}</p>
+  <table v-if="dataMessage == 'Loaded'" class="styled-table">
+    <thead>
+      <tr>
+        <td>Points</td>
+        <td>Article</td>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="newsLink in newsLinks">
+        <td>{{ newsLink.points }}</td>
+        <td>
+          <a :href="newsLink.link" target="_blank">{{ newsLink.title }}</a>
+        </td>
+      </tr>
+    </tbody>
+  </table>
 </template>
+
+<style scoped>
+.styled-table {
+  border-collapse: collapse;
+  margin: 25px 0;
+  font-size: 0.9em;
+  font-family: sans-serif;
+  min-width: 400px;
+  box-shadow: 0 0 20px rgba(0, 0, 0, 0.15);
+}
+.styled-table thead tr {
+  background-color: #009879;
+  color: #ffffff;
+  text-align: left;
+}
+.styled-table th,
+.styled-table td {
+  text-align: left;
+  padding: 12px 15px;
+}
+.styled-table tbody tr {
+  border-bottom: 1px solid #dddddd;
+}
+
+.styled-table tbody tr:nth-of-type(even) {
+  background-color: #f3f3f3;
+}
+
+.styled-table tbody tr:last-of-type {
+  border-bottom: 2px solid #009879;
+}
+</style>
