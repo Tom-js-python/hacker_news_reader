bcd33f98d238f96d80b1911f433ed5540f93c91f
Tom-js-python <tomrembrown@proton.me>
2024-08-12 15:14:13 -0400

    Create Basic Setup for App in Vue

---
+++ frontend/src/components/TheLinksTable.vue
@@ -0,0 +1,5 @@
+<script setup></script>
+
+<template>
+  <p>Data Should be loaded here</p>
+</template>
---
+++ frontend/src/components/TheMenuBar.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { ref } from 'vue'
+
+const data = ref('Data Not Loaded')
+
+function loadData() {
+  data.value = 'Data Loaded!'
+}
+</script>
+
+<template>
+  <button @click="loadData">Load Data</button>
+  <p>{{ data }}</p>
+</template>
+
+<style scoped>
+button {
+  display: inline;
+  margin-bottom: 1em;
+}
+</style>
